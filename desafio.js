aa51588db1c67aa274f182c6ff7a04066d28f986
Davy-Geyson <123582957+Davy-201@users.noreply.github.com>
2023-02-08 20:28:24 -0300

    first commit

---
+++ desafio.js
@@ -0,0 +1,17 @@
+let X = parseFloat(prompt('digite um numero: '));
+
+//Complete os espaços em branco com uma possível solução para o problema
+/*
+if (X <= 25){
+    print("Intervalo [0,25]" );
+} else if(x > 25){
+    print("Intervalo (25,50]");
+} else if(){
+    print("Intervalo (50,75]");
+
+} else if(){
+    print("Intervalo (75,100]");     
+} else{
+    print("Fora de intervalo");
+}*/
+console.log (X)
