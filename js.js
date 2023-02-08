473b49477dd8da1c894c8fe177c7dd9c154e6262
Davy-Geyson <123582957+Davy-201@users.noreply.github.com>
2023-02-07 23:42:49 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,33 @@
+/*
+var nome = "Davy";  //Declaração de variavel
+var sobrenome = "Rodriguez";
+
+console.log("Instrução solta em um arquivo Js");
+// Declaração de função
+function exibirNomeConsole(){
+    console.log("exibir nome: " + nome);
+}
+// Chamada da função
+exibirNomeConsole();
+
+function exibirNomeCompleto(){
+    console.log("nome completo: " + nome + " " + sobrenome);
+}
+function eventoClick(){
+    alert("Você clicou em um botão!");
+}
+//________________________________________________________________________________
+// POO programação orientada a objeto
+var objprofessor = {
+    nome: "Professor Hadson Aguiar",
+    curso: "Formação JavaScript",
+    ministrar_aula: function(){
+        console.log("Ministrando aula de JS");
+    }
+}
+console.log("Objeto", objprofessor);
+console.log(objprofessor.nome);
+console.log(objprofessor.curso);
+console.log(objprofessor.ministrar_aula);
+//__________________________________________________________________________________
+*/
