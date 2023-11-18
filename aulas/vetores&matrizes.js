d544a349c70bfab7e06afebaa3ecbee660846a7f
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-18 13:47:50 -0300

    aula

--- aulas/vetores&matrizes.js
+++ aulas/vetores&matrizes.js
@@ -1,3 +1,9 @@
-let pokemon = ["pikachu", "charmander", "bulbassaur"]
+let nomesPokemon = ["Pikachu" , "Chamander", "Bulbassaur"]
+let timePokemon = [
+  ["pikachu", "M", 1],
+  ["Chamander", "F", 3]
+]
 
-console.log(pokemon[2])
+
+console.log(" O pokemon " + timePokemon[1][0] + " é do sexo " +  timePokemon[1][1] + 
+" e está no nível " + timePokemon[1][2] )
