0e5875160dd6cb38043e0441fcfcadcc749763f0
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-21 16:45:39 -0300

    aula

--- aulas/operadores/logicos.js
+++ aulas/operadores/logicos.js
@@ -3,3 +3,19 @@ let moedasColetadas = 99
 let item = "estrela"
 let resultado = (moedasColetadas >= 100) && (item === "estrela")
 console.log(resultado)
+
+// OR ( || ) - nosso boneco só pode sair se tiver sem chuva OU com guarda Chuva
+
+let tempo = "chuva";
+let guardaChuva = true;
+let podeSair = (tempo !== "chuva" || guardaChuva === true);
+if (tempo === "sol"){
+        let clima = " ensolarado"
+        console.log("o clima esta: " + clima)
+    }else {
+        let clima = "chuvoso"
+        console.log("o clima esta: " + clima)
+    }
+
+console.log("temos um guarda chuva? " + guardaChuva)
+console.log("podemos sair? " + podeSair)
