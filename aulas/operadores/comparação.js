2bdfa8246bb84a1d915871b0b1e5818d4a9b937e
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-21 16:02:22 -0300

    aula

---
+++ aulas/operadores/comparação.js
@@ -0,0 +1,14 @@
+let marca = "Apple"
+console.log(marca !== "Samsung")
+
+// = é atribuição
+// == é para comparar o valor
+// === é para comparar o valor e o formato do conteúdo
+// !== é diferente ?
+
+let CPFPermitido = "222.555.333-02"
+let CPFDoUsuario = "222.555.333-02"
+
+let ehBloqueado = CPFDoUsuario !== CPFPermitido
+
+console.log("é um usuario invalido ? " + ehBloqueado)
