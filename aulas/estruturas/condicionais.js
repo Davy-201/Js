249feed4c4fe6090746bf219c5c384ae38543288
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-22 16:29:14 -0300

    aula

--- aulas/estruturas/condicionais.js
+++ aulas/estruturas/condicionais.js
@@ -1,12 +1,21 @@
-let possuiOvos = false
-let itensComprados = ""
+/*Era uma vez um herói chamado João que estava em uma aventura emocionante para resgatar a princesa perdida em um mundo mágico.
+Mas para alcançar o castelo onde a princesa estava aprisionada, ele precisava atravessar uma floresta cheia de perigos e enigmas.
+Vamos ajudar João a tomar as decisões certas usando uma estrutura IF em JavaScript! 
+Essa estrutura é como um caminho mágico que o nosso herói seguirá. */
 
-if(possuiOvos){
-itensComprados = "Leite"
-}
-else{
-console.log("passou na sessão de congelados")
-itensComprados = "Lasanha congelada"
-}
+let nomeHeroi = 'joão'
+let caminhoEsquerdo = ''
 
-console.log("item comprado: " + itensComprados)
+if (caminhoEsquerdo === 'obstaculo') {
+    // Se o caminho da esquerda tiver um obstáculo, João vai tentar o caminho da direita.
+    seguirCaminhoDireito() 
+      console.log('teste1')
+  } else if (caminhoEsquerdo === 'ponteQuebrada') {
+    // Se o caminho da esquerda tiver uma ponte quebrada, João vai procurar outro caminho.
+    procurarOutroCaminho() 
+      console.log('teste2');
+  } else {
+    // Se não tiver obstáculo nem ponte quebrada no caminho da esquerda, João seguirá por esse caminho.
+    seguirCaminhoEsquerdo()
+      console.log('teste3');
+  };
