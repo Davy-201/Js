6f5ada90890766e8b4eb95a791a556d17209cc6d
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-23 00:16:05 -0300

    aula

--- aulas/estruturas/repetição.js
+++ aulas/estruturas/repetição.js
@@ -1,3 +1,11 @@
 for (let contador = 0; contador <4; contador++){
-    console.log(contador)
+};
+
+let pontosDeVida = 0;
+
+for (let contador = 0; contador < 10; contador++) {
+    pontosDeVida += 1
+    console.log("tomou 1 porção");
 }
+
+console.log(pontosDeVida + " porções tomadas")
