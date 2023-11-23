cac7b4e3be06badfeea9fa5e7b764b029dfe2991
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-23 13:41:59 -0300

    desafio(1)

--- aulas/desafios/nivel-herói.js
+++ aulas/desafios/nivel-herói.js
@@ -1,15 +1,30 @@
 let nameHeroi = "deku";
 let xpHeroi = 0;
 
-for (let contador = 0; contador < 1000; contador +=500 ) {
-    xpHeroi += 500
+for (let contador = 0; contador < 5000; contador +=2500 ) {
+    xpHeroi += 2500
     console.log('herói ganhou '+xpHeroi+' pontos de XP');
-    let nivel = ""
 }
 
-console.log("pontos de experiência "+ xpHeroi)
+console.log(nameHeroi+" tem "+ xpHeroi + " pontos de experiência ")
+
+do {
+    console.log('herói tem agora '+xpHeroi+' pontos de XP');
+    xpHeroi += 500
+}while (xpHeroi <= 6500);
+
+console.log(xpHeroi)
 
 if (xpHeroi <= 1000) {
-    let nivel = 'ferro';
+    let nivel = "ferro";
     console.log('o herói '+ nameHeroi + ' está no nivel ' + nivel);
+} else if (xpHeroi > 1000 && xpHeroi <= 2000) {
+    let nivel = "bronze";
+    console.log('o herói '+ nameHeroi + ' está no nivel ' + nivel)
+} else if (xpHeroi > 2000 && xpHeroi <=5000) {
+    let nivel = "prata";
+    console.log('o herói '+ nameHeroi + ' está no nivel ' + nivel)
+} else if (xpHeroi > 6000 && xpHeroi <=7000) {
+    let nivel = "ouro";
+    console.log('o herói '+ nameHeroi + ' está no nivel ' + nivel)
 }
