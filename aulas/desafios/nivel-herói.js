a05c17e22aa45c9bad0e4410ebd5562a42ec01f4
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-23 14:04:41 -0300

    final

--- aulas/desafios/nivel-herói.js
+++ aulas/desafios/nivel-herói.js
@@ -3,17 +3,15 @@ let xpHeroi = 0;
 
 for (let contador = 0; contador < 5000; contador +=2500 ) {
     xpHeroi += 2500
-    console.log('herói ganhou '+xpHeroi+' pontos de XP');
 }
 
-console.log(nameHeroi+" tem "+ xpHeroi + " pontos de experiência ")
-
 do {
-    console.log('herói tem agora '+xpHeroi+' pontos de XP');
     xpHeroi += 500
 }while (xpHeroi <= 6500);
 
-console.log(xpHeroi)
+while (xpHeroi < 10500) {
+    xpHeroi += 500
+}
 
 if (xpHeroi <= 1000) {
     let nivel = "ferro";
@@ -27,4 +25,18 @@ if (xpHeroi <= 1000) {
 } else if (xpHeroi > 6000 && xpHeroi <=7000) {
     let nivel = "ouro";
     console.log('o herói '+ nameHeroi + ' está no nivel ' + nivel)
-}
+} else if (xpHeroi > 7000 && xpHeroi <=8000) {
+    let nivel = "platina";
+    console.log('o herói '+ nameHeroi + ' está no nivel ' + nivel)
+} else if (xpHeroi > 8000 && xpHeroi <=9000) {
+    let nivel = "Ascendente";
+    console.log('o herói '+ nameHeroi + ' está no nivel ' + nivel)
+} else if (xpHeroi > 9000 && xpHeroi <=10000) {
+    let nivel = "imortal";
+    console.log('o herói '+ nameHeroi + ' está no nivel ' + nivel)
+} else {
+    let nivel = "radiante";
+    console.log('o herói '+ nameHeroi + ' está no nivel ' + nivel)
+};
+
+console.log("XP do Herói: " + xpHeroi);
---
+++ aulas/desafios/teste.js
@@ -0,0 +1,6 @@
+let xpHeroi = 7000
+while ( xpHeroi < 8000) {
+    console.log("olá")
+    xpHeroi += 1000;
+}
+console.log(xpHeroi)
