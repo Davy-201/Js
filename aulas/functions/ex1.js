cd5f642ab2959b9c2ee1ebb2b9c07e19d1db2b76
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-25 20:56:38 -0300

    aula

--- aulas/functions/ex1.js
+++ aulas/functions/ex1.js
@@ -1,21 +1,22 @@
 function main() {
-    torrar("pão integral");
-    ejetandoPão("pão integral");
-    pedido("joão");
+    torrar("pão");
+    ejetandoPão("pão");
+    nota("pão", 9.99);
 }
 
-function torrar(brad) {
-    console.log("torrando pão")
-    console.log("torrada feita com " + brad)
+function torrar(bread) {
+    console.log("torrando " + bread)
 }
 
-function ejetandoPão(brad) {
-    console.log("preparando para ejetar pão")
-    console.log(brad + " ejetado")
+function ejetandoPão(bread) {
+    console.log("preparando para ejetar o " + bread)
+    console.log(bread + " ejetado")
 }
 
-function pedido(name) {
+function nota(bread, valor, name = "cliente") {
+    console.log("torrada feita com " + bread)
     console.log("pedido feito por " + name)
+    console.log("valor total do pedido: " + valor)
 }
 
 main();
