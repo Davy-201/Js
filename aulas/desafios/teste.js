be74d28c6b555e803eed121b907b9e31ca3db107
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-25 17:29:42 -0300

    aula

--- aulas/desafios/teste.js
+++
@@ -1,6 +0,0 @@
-let xpHeroi = 7000
-while ( xpHeroi < 8000) {
-    console.log("olá")
-    xpHeroi += 1000;
-}
-console.log(xpHeroi)
---
+++ aulas/functions/ex1.js
@@ -0,0 +1,12 @@
+
+function torrar() {
+    console.log("torrando pão")
+    ejetandoPão();
+}
+
+function ejetandoPão() {
+    console.log("preparando para ejetar pão")
+    console.log("finalizado")
+}
+
+torrar();
