c8fe1ab4c4a76eb7de3f095a1cec154221ef41a8
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-26 01:21:18 -0300

    final

--- aulas/desafios/ranked-hero.js
+++ aulas/desafios/ranked-hero.js
@@ -1,8 +1,38 @@
 let nameHero = "tony stark";
-let amountVictory = historicPVP(5,2);
+let amountVictory = historicPVP(8,2);
 
 function historicPVP(victory, defeat) {
     return victory-defeat
 }
 
-console.log("o herói tem um saldo de: " + amountVictory + " e esta no nivel bronze")
+for (amountVictory; amountVictory < 105 ; amountVictory++) {
+}
+
+if (amountVictory <= 10) {
+    let nivel = "ferro";
+    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel);
+
+} else if (amountVictory > 10 && amountVictory <= 20) {
+    let nivel = "bronze";
+    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+
+} else if (amountVictory > 20 && amountVictory <=50) {
+    let nivel = "prata";
+    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+
+} else if (amountVictory > 50 && amountVictory <=80) {
+    let nivel = "ouro";
+    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+
+} else if (amountVictory > 80 && amountVictory <=90) {
+    let nivel = "Diamante";
+    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+
+} else if (amountVictory > 90 && amountVictory <=100) {
+    let nivel = "Lendário";
+    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+
+} else {
+    let nivel = "Imortal";
+    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+};
