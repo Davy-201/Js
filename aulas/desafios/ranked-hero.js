5945620f2e0b04a5085411bf1312d4658b494b4a
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-26 00:36:38 -0300

    desafio

---
+++ aulas/desafios/ranked-hero.js
@@ -0,0 +1,8 @@
+let nameHero = "tony stark";
+let amountVictory = historicPVP(5,2);
+
+function historicPVP(victory, defeat) {
+    return victory-defeat
+}
+
+console.log("o herói tem um saldo de: " + amountVictory + " e esta no nivel bronze")
