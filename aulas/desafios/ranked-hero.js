891fcc3324db16b30f4aff86e181d521f4a86988
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-11-29 13:23:59 -0300

    aula

--- aulas/desafios/ranked-hero.js
+++ aulas/desafios/ranked-hero.js
@@ -10,29 +10,29 @@ for (amountVictory; amountVictory < 105 ; amountVictory++) {
 
 if (amountVictory <= 10) {
     let nivel = "ferro";
-    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel);
+    console.log(`o herói ${nameHero} tem um saldo de ${amountVictor} vitórias e está no nivel de ${nivel}`);
 
 } else if (amountVictory > 10 && amountVictory <= 20) {
     let nivel = "bronze";
-    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+    console.log(`o herói ${nameHero} tem um saldo de ${amountVictor} vitórias e está no nivel de ${nivel}`);
 
 } else if (amountVictory > 20 && amountVictory <=50) {
     let nivel = "prata";
-    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+    console.log(`o herói ${nameHero} tem um saldo de ${amountVictor} vitórias e está no nivel de ${nivel}`);
 
 } else if (amountVictory > 50 && amountVictory <=80) {
     let nivel = "ouro";
-    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+    console.log(`o herói ${nameHero} tem um saldo de ${amountVictor} vitórias e está no nivel de ${nivel}`);
 
 } else if (amountVictory > 80 && amountVictory <=90) {
     let nivel = "Diamante";
-    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+    console.log(`o herói ${nameHero} tem um saldo de ${amountVictor} vitórias e está no nivel de ${nivel}`);
 
 } else if (amountVictory > 90 && amountVictory <=100) {
     let nivel = "Lendário";
-    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+    console.log(`o herói ${nameHero} tem um saldo de ${amountVictor} vitórias e está no nivel de ${nivel}`);
 
 } else {
     let nivel = "Imortal";
-    console.log('o herói '+ nameHero + ' tem um saldo de ' +amountVictory+ ' vitórias e está no nivel de ' + nivel)
+    console.log(`o herói ${nameHero} tem um saldo de ${amountVictor} vitórias e está no nivel de ${nivel}`);
 };
