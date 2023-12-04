07bb5a12f94b16938fdc24d241d919c0d01e4c18
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-12-04 12:36:20 -0300

    aula

--- aulas/estruturas/classes.js
+++ aulas/estruturas/classes.js
@@ -3,8 +3,14 @@ class formaDeBolo{
         this.saborDaMassa = saborDaMassa
         this.saborDoRecheio = saborDoRecheio
     }
+
+    escrever(){
+        console.log(`um delicioso bolo com massa de ${this.saborDaMassa} e recheio de ${this.saborDoRecheio}`)
+    }
 };
 
-let boloFesta = new formaDeBolo("massa de chocolate", "recheio de nutella");
+let boloFesta = new formaDeBolo("chocolate", "nutella");
+
+console.log(`qual o é o recheio do bolo? ${boloFesta.saborDoRecheio}`)
 
 console.log(boloFesta);
