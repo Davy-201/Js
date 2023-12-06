533423c4d7c91ea545e368c96bd71889962ba2eb
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-12-06 10:58:44 -0300

    update1

--- aulas/desafios/nivel-herói.js
+++ aulas/desafios/nivel-herói.js
@@ -1,6 +1,6 @@
 const nameHero = "deku";
 let xpHeroi = 0;
-let = ['Ferro', 'Bronze', 'Prata', 'Ouro', 'Platina', 'Ascendente', 'Imortal', 'Radiante']
+let nivel= ['Ferro', 'Bronze', 'Prata', 'Ouro', 'Platina', 'Ascendente', 'Imortal', 'Radiante']
 
 for (let contador = 0; contador < 5000; contador +=2500 ) {
     xpHeroi += 2500
@@ -15,30 +15,35 @@ while (xpHeroi < 10500) {
 }
 
 if (xpHeroi <= 1000) {
-    let nivel = "ferro";
-    console.log(`o herói ${nameHero} está no nivel de ${nivel}`);
+    console.log(`o herói ${nameHero} está no nivel de ${nivel[0]}`);
 ;
-} else if (xpHeroi > 1000 && xpHeroi <= 2000) {
-    let nivel = "bronze";
-    console.log(`o herói ${nameHero} está no nivel de ${nivel}`);
-} else if (xpHeroi > 2000 && xpHeroi <=5000) {
-    let nivel = "prata";
-    console.log(`o herói ${nameHero} está no nivel de ${nivel}`);
-} else if (xpHeroi > 6000 && xpHeroi <=7000) {
-    let nivel = "ouro";
-    console.log(`o herói ${nameHero} está no nivel de ${nivel}`);
-} else if (xpHeroi > 7000 && xpHeroi <=8000) {
-    let nivel = "platina";
-    console.log(`o herói ${nameHero} está no nivel de ${nivel}`);
-} else if (xpHeroi > 8000 && xpHeroi <=9000) {
-    let nivel = "Ascendente";
-    console.log(`o herói ${nameHero} está no nivel de ${nivel}`);
-} else if (xpHeroi > 9000 && xpHeroi <=10000) {
-    let nivel = "imortal";
-    console.log(`o herói ${nameHero} está no nivel de ${nivel}`);
-} else {
-    let nivel = "radiante";
-    console.log(`o herói ${nameHero} está no nivel de ${nivel}`);
-};
+}
+else if (xpHeroi > 1000 && xpHeroi <= 2000) {
+    console.log(`o herói ${nameHero} está no nivel de ${nivel[1]}`);
+
+}
+else if (xpHeroi > 2000 && xpHeroi <=5000) {
+    console.log(`o herói ${nameHero} está no nivel de ${nivel[2]}`);
+
+}
+else if (xpHeroi > 6000 && xpHeroi <=7000) {
+    console.log(`o herói ${nameHero} está no nivel de ${nivel[3]}`);
+
+}
+else if (xpHeroi > 7000 && xpHeroi <=8000) {
+    console.log(`o herói ${nameHero} está no nivel de ${nivel[4]}`);
+
+}
+else if (xpHeroi > 8000 && xpHeroi <=9000) {
+    console.log(`o herói ${nameHero} está no nivel de ${nivel[5]}`);
+
+}
+else if (xpHeroi > 9000 && xpHeroi <=10000) {
+    console.log(`o herói ${nameHero} está no nivel de ${nivel[6]}`);
+
+}
+else {
+    console.log(`o herói ${nameHero} está no nivel de ${nivel[7]}`);
+}
 
 console.log("XP do Herói: " + xpHeroi);
