c5c00a4e6aee74804382f03382d4e2db4e8c0715
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-12-08 17:44:25 -0300

    aula

--- aulas/estruturas/exercicios/valorDaViagem.js
+++ aulas/estruturas/exercicios/valorDaViagem.js
@@ -1,7 +1,18 @@
-const precoDoCombustivel = 5.79;
+const precoDoEtanol = 5.79;
+const precoDaGasolina = 6.69;
 const kmPorLitro = 10;
 const kmRodado = 100;
+const combustivel = 'etanol'
+const isEtanol = (combustivel === 'gasosa');
 
 const consumoCombustivel = kmRodado / kmPorLitro;
-const valorGasto = consumoCombustivel * precoDoCombustivel;
-console.log(valorGasto.toFixed(2));
+
+if (isEtanol){
+    const valorGasto = consumoCombustivel * precoDoEtanol;
+    console.log(valorGasto.toFixed(2));
+} 
+else{
+    const valorGasto = consumoCombustivel * precoDaGasolina;
+    console.log(valorGasto.toFixed(2));
+}
+
