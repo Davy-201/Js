ca6abed306382fc47c4e1e4b2608e058a9a14b7a
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-12-09 15:58:06 -0300

    aula

--- aulas/estruturas/exercicios/imc.js
+++ aulas/estruturas/exercicios/imc.js
@@ -1,21 +1,32 @@
-const peso = 78;
-const altura = 1.87;
-const imc = peso / Math.pow(altura, 2);
+function main() {
+    const weight = 78;
+    const height = 1.87;
+    const imc = calculatorImc(78, 1.87);
 
-if (imc < 18.5) {
-    console.log('Você ésta abaixo do peso')
-} 
-else if (imc >= 18.5 && imc < 25) {
-    console.log('Você ésta com peso normal')
-}  
-else if (imc >= 25 && imc < 30) {
-    console.log('Você ésta acima do peso')
-} 
-else if (imc >= 30 && imc < 40) {
-    console.log('Você ésta obeso')
-} 
-else {
-    console.log('Você ésta com obesidade grave')
+    console.log(`seu peso:${imc.toFixed(1)} ${classifyImc(imc)}`);
 }
 
-console.log(`seu peso: ${imc.toFixed(1)}`);
+function calculatorImc(weight, height) {
+    return weight / Math.pow(height, 2);
+}
+
+function classifyImc(imc) {
+
+    if (imc < 18.5) {
+        return 'Você ésta abaixo do peso'
+    } 
+    else if (imc >= 18.5 && imc < 25) {
+        return 'Você ésta com peso normal'
+    }  
+    else if (imc >= 25 && imc < 30) {
+        return 'Você ésta acima do peso'
+    } 
+    else if (imc >= 30 && imc < 40) {
+        return 'Você ésta obeso'
+    } 
+    else {
+        return 'Você ésta com obesidade grave'
+    }
+}
+
+main();
