94a77ad145f01e64d1c7c0de4184a680c30afbd5
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2023-12-09 13:55:25 -0300

    aula

---
+++ aulas/estruturas/exercicios/valordoproduto.js
@@ -0,0 +1,8 @@
+/* Elabore um algoritimo que calcule o que deve ser pago por um produto, considerando o preço normal
+de etiqueta e a escolha da condição de pagamento.
+Código da condição de pagamento:
+- À vista débito, recebe 10% de desconto
+- À vista em espécie ou pix, recebe 15% de desconto
+- Em 2x preço normal de etiqueta sem juros
+- Acima, preço nromal de etiqueta mais juros de 10% */
+
