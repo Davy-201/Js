1c17b7513ea532e9ac5bb1baa1bd0dacd7444dc4
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2024-01-03 23:49:12 -0300

    exercicio

--- aulas/exercicios/tabuada.js
+++ aulas/exercicios/tabuada.js
@@ -1,9 +1,7 @@
 const numero1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const numero2 = [];
 
-const multiplicacao = numero1 * numero2;
-
-for (let index = 0; index < numero1.length; index++) {
-    const element = array[index];
+for (let index = 0; index <= numero1.length; index++) {
+    const multiplicacao = index * numero1[0];
+    console.log(index);
     
 }
