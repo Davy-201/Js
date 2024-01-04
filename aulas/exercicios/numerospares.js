0a4ec68ba0e74e0839d4f1cc7552e9e8e54c109e
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2024-01-04 00:00:06 -0300

    aula

--- aulas/exercicios/numerospares.js
+++ aulas/exercicios/numerospares.js
@@ -2,7 +2,7 @@ const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 for (let index = 0; index < numeros.length; index++) {
     const numero = numeros[index];
-    if (numero = % 2 === 0) {
+    if (numero % 2 === 0) {
         console.log(numero)
     }
 }
