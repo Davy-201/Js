e14ed92a5920b94c86dbfba6e34ecb8b496377c6
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2024-01-05 22:26:25 -0300

    aula

--- aulas/importação/main.js
+++ aulas/importação/main.js
@@ -1,9 +1,39 @@
 /*
-const funcoes = require('./funçoesauxiliares');
 
-console.log(funcoes.gets());
-*/
+object destructuring
+
+const pessoa = {
+    nome: 'vitor'
+};
+
+const {nome} = pessoa;
+
+const nome = pessoa.nome;
+
 
 const { gets, print} = require('./funçoesauxiliares');
 
 print(gets());
+
+*/
+
+const funcoes = require('./funçoesauxiliares');
+
+const numerosSorteados = [];
+
+for (let index = 0; index < 5; index++) {
+    const numeroSorteado = funcoes.gets();
+    numerosSorteados.push(numeroSorteado);
+    
+}
+
+let maiorValor = 0;
+
+for (let index = 0; index < numerosSorteados.length; index++) {
+    const numeroSorteado = numerosSorteados[index];
+    if(numeroSorteado > maiorValor){
+        maiorValor = numeroSorteado;
+    }
+}
+
+console.log(maiorValor);
